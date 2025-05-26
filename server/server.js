2f59435f4a1acd312699235ab761b7c1db8227a8
rajiv <rajiv.bbdinfy@gmail.com>
2025-05-26 21:39:17 +0530

    Trigger deploy to Azure

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const upload = multer({ storage: storage });
 const apiKey = process.env.API_KEY;
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Enable CORS (allow requests from the frontend)
 app.use(cors());
